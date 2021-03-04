95d2e726ffe401c525f9928bd1a1690783711f82
lionariman <lionariman@gmail.com>
2021-03-04 20:00:36 +0300

    some scripts have been added

---
+++ build.sh
@@ -0,0 +1,7 @@
+docker build -t nginx-image srcs/nginx/
+docker build -t wordpress-image srcs/wordpress/
+docker build -t phpmyadmin-image srcs/phpmyadmin
+#docker build -t ftps-image srcs/ftps/
+#docker build -t grafana-image srcs/grafana/
+#docker build -t influxdb-image srcs/influxdb/
+#docker build -t mysql-image srcs/mysql/
